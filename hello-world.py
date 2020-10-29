af6323c2d547d39c0463dcab40f536144961a439
aradhana14nov <73100123+aradhana14nov@users.noreply.github.com>
2020-10-29 14:26:59 +0530

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,14 +1,12 @@
-from flask import Flask
-import os
+from flask import Flask  # From module flask import class Flask
+app = Flask(__name__)    # Construct an instance of Flask class for our webapp
 
-app = Flask(__name__)
+@app.route('/')   # URL '/' to be handled by main() route handler
+def main():
+    """Say hello"""
+    return 'Hello, world!'
 
-@app.route("/")
-def hello():
-    return "hello world!!"
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,host='0.0.0.0',port=port)
+if __name__ == '__main__':  # Script executed directly?
+    print("Hello World! Built with a Docker file.")
+    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True)  # Launch built-in web server and run this Flask webapp
    
